3b568e85bf8bc0a160202f94119ea0970444d44c
Kieran <kieran.yun@gmail.com>
2022-03-15 09:37:15 -0700

    complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -14,12 +14,10 @@ var items = {};
 
 //Input - the parameter 'text' will be a string that the user writes in the client, ie. "make pancakes".  Callback is what is provided by server. In this case, a successfulPOST request will send us a callback that will return id and text to the client.
 //OUtput - create a file with filename of id and body will be 'text'
-//c/e - ??
 exports.create = (text, callback) => {
   let id;
   counter.getNextUniqueId((err, num) => {
     id = num;
-    //console.log('dataDir: ', exports.dataDir);
     fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err, () => {
       if (err) {
         console.log('error writing file');
@@ -28,14 +26,8 @@ exports.create = (text, callback) => {
       }
     }));
   });
-  //this is asynchronous so ID is not immediately updated
-  //items[id] = text;
-  //console.log(items);
 
 };
-
-// fs.readdir(pathname, callback)
-//callback willbe invoked with (err, [data])
 
 
 exports.readAll = (callback) => {
@@ -47,35 +39,22 @@ exports.readAll = (callback) => {
       callback(null, data.map((item) => { return {'id': item.slice(0, 5), 'text': item.slice(0, 5) }; }));
     }
   });
-  // var data = _.map(items, (text, id) => {
-  //   return { id, text };
-  // });
-  // callback(null, data);
+
 };
 
 exports.readOne = (id, callback) => {
 
   fs.readFile(path.join(exports.dataDir, `${id}.txt`), (err, fileData) => {
-    // console.log(fileData.toString());
     if (err) {
       callback(new Error(`No item with id: ${id}`));
     } else {
       callback(null, {'id': id, 'text': fileData.toString()});
     }
   });
-
-  // var text = items[id];
-  // if (!text) {
-  //   callback(new Error(`No item with id: ${id}`));
-  // } else {
-  //   callback(null, { id, text });
-  // }
 };
 
 exports.update = (id, text, callback) => {
-//must fix this so that the
   fs.readFile(path.join(exports.dataDir, `${id}.txt`), (err, fileData) => {
-  // console.log(fileData.toString());
     if (err) {
       callback(new Error(`No item with id: ${id}`));
     } else {
@@ -83,32 +62,21 @@ exports.update = (id, text, callback) => {
         if (err) {
           callback(new Error(`No item with id: ${id}`));
         } else {
-        //console.log('counter file is: ', exports.counterFile);
           callback(null, { id, text });
         }
       });
     }
   });
-
-
-  // var item = items[id];
-  // if (!item) {
-  //   callback(new Error(`No item with id: ${id}`));
-  // } else {
-  //   items[id] = text;
-  //   callback(null, { id, text });
-  // }
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.rm(path.join(exports.dataDir, `${id}.txt`), (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
